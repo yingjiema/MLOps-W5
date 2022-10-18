fb89d233a4ff84eb7f8e81d867ac1bc912673d69
EC2 Default User <ec2-user@ip-10-0-15-51.ec2.internal>
2022-10-18 06:19:38 +0000

    main.py update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from starlette.responses import StreamingResponse
 from fastapi import FastAPI, File, UploadFile
 import requests
+import json
 
 # Let's generate a new FastAPI app
 # Generate a FastAPI instance called `app` with the title 'Triton Health Check'
@@ -12,6 +13,6 @@ app = FastAPI(title='Face FastAPI')
 @app.get("/", tags=["Health Check"])
 async def root():
     return {
-        "face-bokeh": json.loads(requests.get("http://bokeh:8002/").text).get("message", ""),
-        "face-emotion": json.loads(requests.get("http://emotion:8003/").text).get("message", ""),
+        "face-bokeh": json.loads(requests.get("http://bokeh:8000/").text).get("message", ""),
+        "face-emotion": json.loads(requests.get("http://emotion:8000/").text).get("message", ""),
     } 
